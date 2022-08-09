35cae7adc17f820ecb1ad2566a30861c95390978
Albertus Liberius <w890702@gmail.com>
2022-08-09 16:15:56 +0900

    changed matrix operations to "simd" for speed

--- Sources/UIMotionX/Math.swift
+++ Sources/UIMotionX/Math.swift
@@ -7,14 +7,43 @@
 
 import Foundation
 import CoreMotion
+import simd
 
 public extension CMRotationMatrix{
+    init(_ simdMatrix: simd_double3x3){
+        let cols = simdMatrix.columns
+        self.init(
+            m11: cols.0.x, m12: cols.1.x, m13: cols.2.x,
+            m21: cols.0.y, m22: cols.1.y, m23: cols.2.y,
+            m31: cols.0.z, m32: cols.1.z, m33: cols.2.z)
+    }
+    var simd_rows: (simd_double3, simd_double3, simd_double3){
+        (
+            simd_make_double3(m11, m12, m13),
+            simd_make_double3(m21, m22, m23),
+            simd_make_double3(m31, m32, m33)
+        )
+    }
+    var simd_columns: (simd_double3, simd_double3, simd_double3){
+        (
+            simd_make_double3(m11, m21, m31),
+            simd_make_double3(m12, m22, m32),
+            simd_make_double3(m13, m23, m33)
+        )
+    }
+    var simd_matrix: simd_double3x3{
+        .init(columns: simd_columns)
+    }
     var determinant: Double{
+        /*
         m11 * (m22 * m33 - m23 * m32)
         + m12 * (m23 * m31 - m21 * m33)
         + m13 * (m21 * m32 - m22 * m31)
+         */
+        simd_matrix.determinant
     }
     var inverseMatrix: CMRotationMatrix{
+        /*
         let det = determinant
         return CMRotationMatrix(
             m11: (m22 * m33 - m23 * m32) / det,
@@ -26,18 +55,22 @@ public extension CMRotationMatrix{
             m31: (m21 * m32 - m22 * m31) / det,
             m32: (m12 * m31 - m11 * m32) / det,
             m33: (m11 * m22 - m12 * m21) / det)
+         */
+        Self(simd_matrix.inverse)
     }
     func multiplying(by aMatrix: CMRotationMatrix) -> CMRotationMatrix{
-        CMRotationMatrix(
-            m11: m11 * aMatrix.m11 + m12 * aMatrix.m21 + m13 * aMatrix.m31,
-            m12: m11 * aMatrix.m12 + m12 * aMatrix.m22 + m13 * aMatrix.m32,
-            m13: m11 * aMatrix.m13 + m12 * aMatrix.m23 + m13 * aMatrix.m33,
-            m21: m21 * aMatrix.m11 + m22 * aMatrix.m21 + m23 * aMatrix.m31,
-            m22: m21 * aMatrix.m12 + m22 * aMatrix.m22 + m23 * aMatrix.m32,
-            m23: m21 * aMatrix.m13 + m22 * aMatrix.m23 + m23 * aMatrix.m33,
-            m31: m31 * aMatrix.m11 + m32 * aMatrix.m21 + m33 * aMatrix.m31,
-            m32: m31 * aMatrix.m12 + m32 * aMatrix.m22 + m33 * aMatrix.m32,
-            m33: m31 * aMatrix.m13 + m32 * aMatrix.m23 + m33 * aMatrix.m33)
+        let myrows = simd_rows
+        let yourcolumns = aMatrix.simd_columns
+        return CMRotationMatrix(
+            m11: simd_dot(myrows.0, yourcolumns.0),
+            m12: simd_dot(myrows.0, yourcolumns.1),
+            m13: simd_dot(myrows.0, yourcolumns.2),
+            m21: simd_dot(myrows.1, yourcolumns.0),
+            m22: simd_dot(myrows.1, yourcolumns.1),
+            m23: simd_dot(myrows.1, yourcolumns.2),
+            m31: simd_dot(myrows.2, yourcolumns.0),
+            m32: simd_dot(myrows.2, yourcolumns.1),
+            m33: simd_dot(myrows.2, yourcolumns.2))
     }
 }
 
